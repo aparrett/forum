a164c1b096167e72c02047f0dd13d5183da848cf
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-06 19:56:15 -0600

    Show replies on thread page

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,6 +12,16 @@
                         <div>{{ $thread->body }}</div>
                     </article>
                 </div>
+                <div class="card">
+                    <div class="card-body">
+                         @foreach ($thread->replies as $reply)
+                            <div class="panel-body">
+                                {{ $reply->body }}
+                            </div>
+                            <hr>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -6,26 +6,38 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ThreadsTest extends TestCase
+class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
 
     /** @test */
     public function a_user_can_browse_threads()
     {
-        $thread = factory('App\Thread')->create();
-        
-        $response = $this->get('/threads');
-        $response->assertSee($thread->title);
-
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
 
-        $response=$this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+    /** @test */
+    public function a_user_can_read_replies_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+            
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
     }
 }
