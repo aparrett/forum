e3cc0bedec210046cea76dfd506d06c2483e4dbc
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-12 20:43:39 -0500

    Users can filter threads by name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,16 @@
                         <li><a href="/threads">All Threads</a></li>
                     </ul>
 
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="/threads">All Threads</a></li>
+                            @if (auth()->check())
+                                <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
+                            @endif
+                        </ul>
+                    </li>
+
                     <li>
                         <a href="/threads/create">New Thread</a>
                     </li>
